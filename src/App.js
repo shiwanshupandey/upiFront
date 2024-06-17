d4f697e65cb24d80fc77f70ed466fad772ab69dc
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-06-17 18:34:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Form from './Components/Form'; 
 import Formpage2 from './Components/Formpage2';
+import FormIntro from './Components/FormIntro.js';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Form />} /> {/* This is your route to the home page */}
+          <Route path="/" element={<FormIntro/>}/>
+          <Route path="/page1" element={<Form />} /> 
           <Route path="/page2" element={<Formpage2 />} />
         </Routes>
       </div>
