36a26dcb1fcdc2d3c80f908217baf9b72db2ebc1
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2024-07-11 12:26:05 +0530

    Update FormIntro.js

--- src/Components/FormIntro.js
+++ src/Components/FormIntro.js
@@ -5,10 +5,11 @@ import FormData from "./FormData.json";
 import Button from './Button/Button';
 
 const FormIntro = () => {
-  const imageUrl = "./Photouse.png"; // Replace with the path to your image
+  const imageUrl = "./Photouse.webp"; // Replace with the path to your image
 
   return (
     <div className='Intro-container'>
+      {/* <img src="./logo.webp" alt='logo' className='ogcs-logo'/> */}
       
       <div className="image-responsive">
         <img
@@ -26,14 +27,31 @@ const FormIntro = () => {
         </div>
         
         <h1 className='WhyHeading'>Event Details</h1>
-        <p className='EventDetails'>
-          <strong>When:</strong> 20th July, 2024 at 3:00 PM<br/>
-          <strong>Duration:</strong> 3 Hours (Live Online Program on Zoom)<br/>
-          <strong>Training Fee:</strong> ₹199/-<br/>
-          <strong>Program Language:</strong> A mix of English and Hindi for better understanding, ensuring it directly touches your heart and mind.
-        </p>
+        <div className='EventDetails'>
+  <div className='detail-item'>
+    <img src='./icon1.webp' alt='Calendar icon' className='icon-FormIntro' />
+    <h1 className='DetailsEventHeading'>When:</h1>
+    <p className='DetailsEventPara'>20th July, 2024 at 3:00 PM</p>
+  </div>
+  <div className='detail-item'>
+    <img src='./icon3.webp' alt='Money icon' className='icon-FormIntro' />
+    <h1 className='DetailsEventHeading'>Training Fee:</h1>
+    <p className='DetailsEventPara'>₹199/-</p>
+  </div>
+  <div className='detail-item'>
+    <img src='./icon2.webp' alt='Clock icon' className='icon-FormIntro' />
+    <h1 className='DetailsEventHeading'>Duration:</h1>
+    <p className='DetailsEventPara'> 3 Hours (Live Online Program on Zoom)</p>
+  </div>
+  <div className='detail-item'>
+    <img src='./icon4.webp' alt='Language icon' className='icon-FormIntro' />
+    <h1 className='DetailsEventHeading'>Program Language:</h1>
+    <p className='DetailsEventPara'> A mix of English and Hindi for better understanding, ensuring it directly touches your heart and mind.</p>
+  </div>
+</div>
+
         <h1 className='WhyHeading2'>Program Insight :</h1>
-        <p className='WhyTrainer'>Participants will master the art of confidently handling interviews and presenting themselves with impact. They will learn to maintain assertive body language and employ powerful strategies to overcome various interview challenges. This program will also focus on effectively showcasing their skills and knowledge, empowering participants to highlight their strengths compellingly during interviews.</p>
+        <p className='WhyPara'>Participants will master the art of confidently handling interviews and presenting themselves with impact. They will learn to maintain assertive body language and employ powerful strategies to overcome various interview challenges. This program will also focus on effectively showcasing their skills and knowledge, empowering participants to highlight their strengths compellingly during interviews.</p>
         
         <Faqs data={FormData.faqs}/>
         {/* <a href="https://www.ogcs.co.in/about-us" target="_blank" rel="noopener noreferrer" className='Link-pages'><FontAwesomeIcon icon={faArrowRight} /> About Us</a> */}
